360a1e5c45e8f7251b84b1f9f337674f2e235808
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2021-01-24 23:05:07 -0300

    configurando a porta

--- API imc/api/server.js
+++ API imc/api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const paciente = require('./data/pacientes.js');
+let porta = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/pacientes', (req, res)=>{
     res.json( paciente() );
 });
-// app.listen(3000);
+
+app.listen(porta);
