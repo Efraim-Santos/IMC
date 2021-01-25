96bdc71651cb416fe2f7ad4b123e25d927beebbb
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2021-01-25 00:25:20 -0300

    montando minha api

--- API imc/api/server.js
+++ API imc/api/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const paciente = require('./data/pacientes.js');
+const paciente = require('./data/paciente.json');
 let porta = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/pacientes', (req, res)=>{
-    res.json( paciente() );
+    res.json( paciente );
 });
 
 app.listen(porta);
