a9f7279ba68e8b93fcbace6694b1e96dddc98be0
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2021-01-25 23:41:28 -0300

    implementando post na api

--- API imc/api/server.js
+++ API imc/api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const paciente = require('./data/paciente.json');
 const cors = require('cors');
+const fs = require('fs');
+const { json } = require('express');
 
 let porta = process.env.PORT || 3000;
 
@@ -16,6 +18,25 @@ const opcoesCors = {
 
 app.get('/pacientes', cors(opcoesCors), (req, res)=>{
     res.json( paciente );
+});
+
+app.post('/adicionarPaciente', (req, res) => {
+    const { nome, peso, altura, imc } = req.body;
+    
+    let dados = {
+        nome,
+        peso,
+        altura,
+        imc
+    };
+
+    paciente.push(dados);
+    
+    fs.writeFile('./api/data/paciente.json', JSON.stringify(paciente), (err) => {
+        if(err) throw err;
+        console.log("Salvo");
+    });
+    res.json("ok");
 });
 
 app.listen(porta, ()=> {
