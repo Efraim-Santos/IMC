075da006646ce41e18deb679aa0ce52b3457baf6
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2021-01-25 22:40:42 -0300

    implementando cors na api

--- API imc/api/server.js
+++ API imc/api/server.js
@@ -8,15 +8,19 @@ let porta = process.env.PORT || 3000;
 app.use(express.json());
 
 const opcoesCors = {
-    origin: 'Access-Control-Allow-Origin: *',
-    optionsSucessStatus: 200
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 };
 
 app.get('/pacientes', cors(opcoesCors), (req, res)=>{
     res.json( paciente );
 });
 
-app.listen(porta);
+app.listen(porta, ()=> {
+    console.log("Cors habilitado");
+});
 
 module.exports = () => {
  return app;
